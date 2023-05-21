050cc017e88e078bed9312d2f5cfaa5be5a185d7
OMedeirosg <gabrielmed00@hotmail.com>
2023-05-21 19:58:16 -0300

    abertura do localhost

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('TypeScript')
+import express from 'express'
+
+const server =express()
+
+
+server.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+server.listen(3333,()=>{
+    console.log('Server started on port 3333')
+})
