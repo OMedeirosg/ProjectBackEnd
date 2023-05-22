845056fb5c25205675e357636f06b9dbd46e2dbb
OMedeirosg <gabrielmed00@hotmail.com>
2023-05-21 22:41:29 -0300

    api feita

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import express from 'express'
+import express,{Request} from 'express'
+import { userRouter } from './modules/user/userRouter.js'
 
-const server =express()
+const server = express()
+server.use(express.json())
 
-
-server.get('/',(req,res)=>{
+server.get('/',(req, res)=>{
     res.send('Hello World')
 })
 
-server.listen(3333,()=>{
+
+server.use('/user',userRouter)
+
+server.listen(3333, () => {
     console.log('Server started on port 3333')
 })
+
